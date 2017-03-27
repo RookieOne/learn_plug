06521926f881d2ac27efb70467ddd1184d58b266
Jonathan Birkholz <rookieone@gmail.com>
2017-03-27 01:38:41 -0500

    Add howdy route

--- lib/learn_plug/my_router.ex
+++ lib/learn_plug/my_router.ex
@@ -4,6 +4,10 @@ defmodule MyRouter do
   plug :match
   plug :dispatch
 
+  match "/howdy" do
+    Plug.Conn.send_resp(conn, 200, "Howdy")
+  end
+
   match _ do
     Plug.Conn.send_resp(conn, 200, "Hello World from Router")
   end
