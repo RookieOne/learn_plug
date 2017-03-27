8fdd15e8fa5b34414f36b20ff04f9d6ff1c143fe
Jonathan Birkholz <rookieone@gmail.com>
2017-03-27 13:17:00 -0500

    Fetch query parameters

--- lib/learn_plug/my_router.ex
+++ lib/learn_plug/my_router.ex
@@ -1,6 +1,8 @@
 defmodule MyRouter do
   use Plug.Router
 
+  import Plug.Conn
+
   plug :match
   plug :dispatch
 
@@ -14,6 +16,12 @@ defmodule MyRouter do
 
   post "/widgets" do
     Plug.Conn.send_resp(conn, 200, "Add widget")
+  end
+
+  post "/tweet" do
+    conn = fetch_query_params(conn)
+    %{"message" => tweet} = conn.params
+    send_resp(conn, 200, "You just tweet'd : " <> tweet)
   end
 
   match _ do
