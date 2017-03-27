86f8d51e8cd19a5ecbe1c110b64e4aca3bc98e4a
Jonathan Birkholz <rookieone@gmail.com>
2017-03-27 14:36:00 -0500

    Log request time

--- lib/learn_plug/log_request_plug.ex
+++ lib/learn_plug/log_request_plug.ex
@@ -1,10 +1,18 @@
 defmodule LogRequestPlug do
   require Logger
 
+  import Plug.Conn
+
   def init(opts), do: opts
 
   def call(conn, _opts) do
     Logger.info("#{conn.method} #{conn.request_path}")
-    conn
+    start_time = :erlang.monotonic_time(:micro_seconds)
+    register_before_send(conn, fn conn ->
+      end_time = :erlang.monotonic_time(:micro_seconds)
+      duration = end_time - start_time
+      Logger.info("Request took #{duration} microseconds")
+      conn
+    end)
   end
 end
