d88adba05f0c93b4859852ca5c8e7af3b63d7f10
Jonathan Birkholz <rookieone@gmail.com>
2017-03-27 13:14:32 -0500

    Add get and post route

--- lib/learn_plug/my_router.ex
+++ lib/learn_plug/my_router.ex
@@ -8,6 +8,14 @@ defmodule MyRouter do
     Plug.Conn.send_resp(conn, 200, "Howdy")
   end
 
+  get "/widgets" do
+    Plug.Conn.send_resp(conn, 200, "Return widgets")
+  end
+
+  post "/widgets" do
+    Plug.Conn.send_resp(conn, 200, "Add widget")
+  end
+
   match _ do
     Plug.Conn.send_resp(conn, 200, "Hello World from Router")
   end
